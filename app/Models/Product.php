569f191d94f9c5310de0edcb8903b0b0fc3a644a
Mbroka-Shrife <coolalshareef1@gmail.com>
2026-01-11 15:37:46 +0200

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    protected $fillable = ['name', 'description', 'price', 'quantity', 'image', 'is_available', 'user_id', 'category_id'];
+
+public function user() {
+    return $this->belongsTo(User::class); // المزارع صاحب المنتج
+}
+
+public function category() {
+    return $this->belongsTo(Category::class);
+}
+}
---
+++ database/migrations/2026_01_11_125900_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+       $table->id();
+        $table->string('name');
+        $table->text('description');
+        $table->string('image')->nullable();
+        $table->decimal('price', 8, 2);
+        $table->integer('quantity'); 
+        $table->boolean('is_available')->default(true);
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        $table->foreignId('category_id')->constrained('categories');
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
